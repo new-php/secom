8b2190bdcdb821d8e591b51aa5a7eb397a23a3b5
josh <josua.hdz.reza@gmail.comh>
2020-10-03 16:16:12 -0600

    made acc type combobox read only

--- src/views/sign_up_window.py
+++ src/views/sign_up_window.py
@@ -11,11 +11,11 @@ class SignUpWind(ttk.Frame):
 
         # Create label.
         title_lbl = ttk.Label(self,
-                                  text="Crear cuenta nueva",
-                                  font=tkf.Font(family="Helvetica", size=15))
+                              text="Crear cuenta nueva",
+                              font=tkf.Font(family="Helvetica", size=15))
         first_name_lbl = ttk.Label(self,
-                                    text="Nombre:",
-                                    font=tkf.Font(family="Helvetica", size=10))
+                                   text="Nombre:",
+                                   font=tkf.Font(family="Helvetica", size=10))
         second_name_lbl = ttk.Label(self,
                                     text="Segundo nombre:",
                                     font=tkf.Font(family="Helvetica", size=10))
@@ -26,17 +26,17 @@ class SignUpWind(ttk.Frame):
                                     text="Apellido materno:",
                                     font=tkf.Font(family="Helvetica", size=10))
         user_lbl = ttk.Label(self,
-                                 text="Usuario:",
-                                 font=tkf.Font(family="Helvetica", size=10))
+                             text="Usuario:",
+                             font=tkf.Font(family="Helvetica", size=10))
         pswd_lbl = ttk.Label(self,
-                                 text="Contraseña:",
-                                 font=tkf.Font(family="Helvetica", size=10))
+                             text="Contraseña:",
+                             font=tkf.Font(family="Helvetica", size=10))
         pswd_confirm_lbl = ttk.Label(self,
-                                        text="Confirme contraseña:",
-                                        font=tkf.Font(family="Helvetica", size=10))
+                                     text="Confirme contraseña:",
+                                     font=tkf.Font(family="Helvetica", size=10))
         safewrd_lbl = ttk.Label(self,
-                                    text="Palabra salvavidas:",
-                                    font=tkf.Font(family="Helvetica", size=10))
+                                text="Palabra salvavidas:",
+                                font=tkf.Font(family="Helvetica", size=10))
         acc_type_lbl = ttk.Label(self,
                                  text="Tipo de cuenta:",
                                  font=tkf.Font(family="Helvetica", size=10))
@@ -52,7 +52,8 @@ class SignUpWind(ttk.Frame):
         #Creation of cobobox.
         self.acc_type_value = tk.StringVar()
         acc_type_cbx = ttk.Combobox(self,
-                                         textvariable=self.acc_type_value)
+                                    state="readonly",
+                                    textvariable=self.acc_type_value)
         acc_type_cbx['values'] = ("Planeacion", "Almlacen")
         # Creation of button.
         back_btn = ttk.Button(self,
@@ -67,87 +68,87 @@ class SignUpWind(ttk.Frame):
 
         # Places labels.
         title_lbl.grid(row=0,
-                           column=1,
-                           padx=120,
-                           pady=10,
-                           sticky=tk.NSEW)
+                       column=1,
+                       padx=120,
+                       pady=10,
+                       sticky=tk.NSEW)
         first_name_lbl.grid(row=1,
                             column=1,
                             padx=70,
                             sticky=tk.W)
         second_name_lbl.grid(row=3,
-                            column=1,
-                            padx=70,
-                            sticky=tk.W)
-        f_last_name_lbl.grid(row=5,
-                            column=1,
-                            padx=71,
-                            sticky=tk.W)
-        m_last_name_lbl.grid(row=7,
-                            column=1,
-                            padx=71,
-                            sticky=tk.W)
-        user_lbl.grid(row=1,
-                          column=1,
-                          padx=145,
-                          sticky=tk.E)
-        pswd_lbl.grid(row=3,
-                          column=1,
-                          padx=120,
-                          sticky=tk.E)
-        pswd_confirm_lbl.grid(row=5,
-                                 column=1,
-                                 padx=70,
-                                 sticky=tk.E)
-        safewrd_lbl.grid(row=7,
                              column=1,
-                             padx=80,
-                             sticky=tk.E)
+                             padx=70,
+                             sticky=tk.W)
+        f_last_name_lbl.grid(row=5,
+                             column=1,
+                             padx=71,
+                             sticky=tk.W)
+        m_last_name_lbl.grid(row=7,
+                             column=1,
+                             padx=71,
+                             sticky=tk.W)
+        user_lbl.grid(row=1,
+                      column=1,
+                      padx=145,
+                      sticky=tk.E)
+        pswd_lbl.grid(row=3,
+                      column=1,
+                      padx=120,
+                      sticky=tk.E)
+        pswd_confirm_lbl.grid(row=5,
+                              column=1,
+                              padx=70,
+                              sticky=tk.E)
+        safewrd_lbl.grid(row=7,
+                         column=1,
+                         padx=80,
+                         sticky=tk.E)
         acc_type_lbl.grid(row=9,
                           column=1,
                           pady=5,
                           sticky=tk.NS)
         # Places entries (string inputs).
         self.first_name_ety.grid(row=2,
-                          column=1,
-                          padx=70,
-                          pady=5,
-                          sticky=tk.W)
-        self.second_name_ety.grid(row=4,
-                          column=1,
-                          padx=70,
-                          pady=5,
-                          sticky=tk.W)
-        self.f_last_name_ety.grid(row=6,
-                          column=1,
-                          padx=70,
-                          pady=5,
-                          sticky=tk.W)
-        self.m_last_name_ety.grid(row=8,
-                          column=1,
-                          padx=70,
-                          pady=5,
-                          sticky=tk.W)
-        self.user_ety.grid(row=2,
-                          column=1,
-                          padx=70,
-                          pady=5,
-                          sticky=tk.E)
-        self.pswd_ety.grid(row=4,
-                          column=1,
-                          padx=70,
-                          pady=5,
-                          sticky=tk.E)
-        self.pswd_confirm_ety.grid(row=6,
                                  column=1,
                                  padx=70,
                                  pady=5,
-                                 sticky=tk.E)
+                                 sticky=tk.W)
+        self.second_name_ety.grid(row=4,
+                                  column=1,
+                                  padx=70,
+                                  pady=5,
+                                  sticky=tk.W)
+        self.f_last_name_ety.grid(row=6,
+                                  column=1,
+                                  padx=70,
+                                  pady=5,
+                                  sticky=tk.W)
+        self.m_last_name_ety.grid(row=8,
+                                  column=1,
+                                  padx=70,
+                                  pady=5,
+                                  sticky=tk.W)
+        self.user_ety.grid(row=2,
+                           column=1,
+                           padx=70,
+                           pady=5,
+                           sticky=tk.E)
+        self.pswd_ety.grid(row=4,
+                           column=1,
+                           padx=70,
+                           pady=5,
+                           sticky=tk.E)
+        self.pswd_confirm_ety.grid(row=6,
+                                   column=1,
+                                   padx=70,
+                                   pady=5,
+                                   sticky=tk.E)
         self.hint_ety.grid(row=8,
-                             column=1,
-                             padx=70,
-                             pady=5,
-                             sticky=tk.E)
+                           column=1,
+                           padx=70,
+                           pady=5,
+                          sticky=tk.E)
         # Palcing combobox.
         acc_type_cbx.grid(row=10,
                           column=1,
@@ -156,15 +157,15 @@ class SignUpWind(ttk.Frame):
         acc_type_cbx.current(0)
         # Places buttons
         back_btn.grid(row=12, 
-                           column=1,
-                           padx=40,
-                           pady=5,
-                           sticky=tk.NS)
+                      column=1,
+                      padx=40,
+                      pady=5,
+                      sticky=tk.NS)
         create_btn.grid(row=11,
-                             column=1,
-                             padx=40,
-                             pady=5,
-                             sticky=tk.NS)
+                        column=1,
+                        padx=40,
+                        pady=5,
+                        sticky=tk.NS)
 
 
     def send_info(self):
