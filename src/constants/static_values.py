e6b995718cec6407cee70036977955b811408426
josh <josh.hdz.reza@gmail.com>
2020-11-07 20:53:29 -0800

    create file for all constatn variables in app

---
+++ src/constants/static_values.py
@@ -0,0 +1,18 @@
+import os
+############################## Window Size #####################################
+# LOG IN.
+LIWIND_WIDTH = 400
+LIWIND_HEIGHT = 225
+# SIGN UP
+SUWIND_WIDTH = 800
+SUWIND_HEIGHT = 500
+################################################################################
+
+# Img's adress.
+app_mini_logo = os.path.abspath("src/assets/imgs/icon.png")
+
+# User account types.
+acc_types = {
+    'Almacen': 1,
+    'Planeacion': 2
+}
