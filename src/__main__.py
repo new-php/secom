2b8fedc543e0390a5033288a6100e19a23b58c0b
josh-hdz <joshua.hdz.reza@gmail.com>
2021-01-30 17:46:33 -0800

    automatic log in for testing NEED TO REMOVE WHEN TESTING DONE

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,13 @@
+import os
 from views.root import Root
 
 
 if __name__ == '__main__':
     root = Root()
+
+    root.views[root._conv("LIWind")].user_ety.insert(0, "josh-hdz")
+    root.views[root._conv("LIWind")].pswd_ety.insert(0, os.environ.get("DB_PSWD"))
+    root.views[root._conv("LIWind")].logIn_btn.invoke()
+
     
     root.mainloop()
