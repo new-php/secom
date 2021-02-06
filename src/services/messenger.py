a0208c94d945a98df829f0849cf3c8079bd0c4ed
josh-hdz <joshua.hdz.reza@gmail.com>
2021-02-05 19:00:45 -0800

    deleted now unused create user function replaced by insert_into

--- src/services/messenger.py
+++ src/services/messenger.py
@@ -22,29 +22,6 @@ class Messenger:
         """
         cursor = self.DB.cursor(buffered=True)
         
-        cursor.execute(
-            "INSERT INTO user ( "\
-                "user_name, "\
-                "pswd, "\
-                "hint, "\
-                "user_type, "\
-                "first_name, "\
-                "second_name, "\
-                "f_last_name, "\
-                "m_last_name) "\
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-            (info[0],   # user_name. 
-            bcrypt.hashpw(info[1].encode('utf-8'), bcrypt.gensalt()),   #Hashing the password.
-            info[2],    # hint.
-            info[3],    # user_type.
-            info[4],    # first_name.
-            info[5],    # second_name.
-            info[6],    # f_last_name.
-            info[7])    # m_last_name.
-        )
-
-        self.DB.commit()
-        cursor.close()
 
     def _modify_args(f):
         def wrapper(self, table, *args):
